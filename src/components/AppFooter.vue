aab74510f7a124199b78e0fa5e463a885bb79e75
hideto tsuzuki <ma_kux@icloud.com>
2023-02-19 15:52:05 +0900

    フッター作成

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-footer
+      dark
+      absolute
+    >
+      <div class="footer">
+        Copyright test 2023. All right reserved
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+  
+  
+  <style lang="scss" scoped>
+  .footer {
+    margin: auto;
+  }
+  </style>
