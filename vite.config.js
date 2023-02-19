09fab016a21874cba8e1bf8bd1bb0a5abeb273a8
hideto tsuzuki <ma_kux@icloud.com>
2023-02-19 16:47:41 +0900

    config設定

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify' // 追記された
+import vuetify from "vite-plugin-vuetify" // 追記された
 
 // https://vitejs.dev/config/
 export default defineConfig({
