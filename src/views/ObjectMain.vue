1d0bdb3a0675c9a860115d4985997fec9ff57207
hideto tsuzuki <ma_kux@icloud.com>
2023-04-05 16:55:14 +0900

    APIからモデルリスト受け取れるように修正

--- src/views/ObjectMain.vue
+++ src/views/ObjectMain.vue
@@ -1,17 +1,17 @@
 <script setup>
 
-// import { ref, watch, reactive, computed } from "vue"
-// import { useRoute, useRouter } from 'vue-router'
-// // import { useRouter } from 'vue-router'
+import { ref, watch, reactive, computed } from "vue"
+import { useRoute, useRouter } from 'vue-router'
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
 // // vueライブラリー定義
-// const router = useRouter()
-// const route = useRoute()
+const router = useRouter()
 
 
 // // 変数定義
+let imageList = []
 
-// const username = ref("")
 // const password = ref("")
 // const loading = ref(false)
 // const valid = ref(false)
@@ -51,32 +51,115 @@
 
 // }
 
-// 後々APIから受け取れるようにする
-const imageList = ["EfficientDet", "YoloV7", "other_model"]
+// 初期ロード
+
+// new Promise((resolve, reject) => {
+//   reject();
+// })
+//   .then(() => {
+//     if (typeof localStorage.token !== "undefined") {
+//       console.log(localStorage.token)
+//     }
+//   })
+//   .catch(() => {
+//     Swal.fire({
+//     type: 'warning',
+//     title: 'Error',
+//     text: 'セッションが切れてます。再ログインして下さい。',
+//     showConfirmButton:false,
+//     showCloseButton:false,
+//     timer:3000
+//     })
+//     router.push('/auth')
+//   });
+
+const load = async () => {
+  let modelList = ref([])
+  if (typeof localStorage.token !== "undefined") {
+    console.log(localStorage.token)
+    // 後々APIから受け取れるようにする
+    return await axios
+          .get('http://127.0.0.1:8000/api/object_detection_model/object_detection_model_list/')
+          .then((res) => {
+            if (res.data.result_code === 0) {
+              // 入力値取得
+              modelList = res.data.detail.result
+              } else {
+                Swal.fire({
+                type: 'warning',
+                title: 'Error',
+                text: ErrorMessage,
+                showConfirmButton:false,
+                showCloseButton:false,
+                })
+              }
+              return modelList
+            }).catch((err) => {
+              Swal.fire({
+              type: 'warning' + err,
+              title: 'Error',
+              text: 'サーバーエラー',
+              showConfirmButton:false,
+              showCloseButton:false,
+              })
+            }).finally(() => {})
+  } else {
+    await Swal.fire({
+      type: 'warning',
+      title: 'Error',
+      text: 'セッションが切れてます。再ログインして下さい。',
+      showConfirmButton:false,
+      showCloseButton:false,
+      timer:3000
+      })
+      router.push('/auth')
+    }
+  }
+
+const reqestList = await load()
+
+for (const i of reqestList) {
+  imageList.push(i["object_detection_model_name"])
+}
+
+
+const modelClick = (val) => {
+  if (imageList[val] === "EfficientDet") {
+    console.log(imageList[val])
+  } else if (imageList[val] === "YoloV7") {
+    console.log(imageList[val])
+  } else if (imageList[val] === "YoloV5") {
+    console.log(imageList[val])
+  }
+}
+
+
 </script>
 
 
+
 <template>
+  <Suspense>
     <v-item-group selected-class="bg-primary">
       <v-container>
         <v-row>
           <v-col
-            v-for="n in imageList.length"
-            :key="n"
+            v-for="(value, keys) in imageList"
+            :key="keys"
             cols="12"
             md="4"
           >
-            <v-item v-slot="{ isSelected, selectedClass, toggle }">
+            <v-item v-slot="{ selectedClass }">
               <v-card
                 :class="['d-flex align-center', selectedClass]"
                 dark
                 height="200"
-                @click="toggle"
+                @click="modelClick(keys)"
               >
                 <div
                   class="text-h3 flex-grow-1 text-center"
                 >
-                  {{ isSelected ? 'Selected' : 'Click Me!' }}
+                  {{ value }}
                 </div>
               </v-card>
             </v-item>
@@ -84,4 +167,5 @@ const imageList = ["EfficientDet", "YoloV7", "other_model"]
         </v-row>
       </v-container>
     </v-item-group>
-  </template>
+  </Suspense>
+</template>
