3f6d61e7f82ae6dde47077cee617f8c4c3fd2b5e
hideto tsuzuki <ma_kux@icloud.com>
2023-06-10 11:23:55 +0900

    新規・更新及び削除処理実装

--- src/views/ProjectList.vue
+++ src/views/ProjectList.vue
@@ -1,9 +1,8 @@
 <script setup>
 
-import { ref, watch, reactive, computed } from "vue";
+import { ref, watch, reactive, computed, watchEffect } from "vue";
 import { useRoute, useRouter } from 'vue-router';
 import Swal from 'sweetalert2';
-import axios from 'axios';
 import post from '../components/post';
 import request from "../components/request";
 import Query from "../components/queryList";
@@ -18,10 +17,11 @@ const username = sessionStorage.getItem('username')
 const token = sessionStorage.getItem('token')
 const dialog = ref(false)
 const createDialog = ref(false)
-const hasSaved = ref(false)
+const deleteSaveDialog = ref(false)
 const isEditing = ref(false)
-const model = ref(false)
 const project_name = ref("")
+const resDataCheckedAdd = reactive([])
+const deleteSaveFlag = ref(false)
 
 const queryPrames = router.currentRoute.value.query
 
@@ -30,9 +30,12 @@ let query = new Query("username", "token", "user_id", "model_name", "id").queryL
 
 // API定義
 const requestAPI = "http://127.0.0.1:8000/api/project/project_list/" + query
+const updateCreateAPI = "http://127.0.0.1:8000/api/project/project_create_or_update/"
+const deleteAPI = "http://127.0.0.1:8000/api/project/project_delete/"
 
 
 let resData
+let requestData = []
 let evaluationindexPush
 let getDetails
 let detailsFrom = {
@@ -45,9 +48,65 @@ let returnScreen
 let create
 let save
 let close
+let deleted
+let beforeData
+
+
+// グローバル関数定義
+
+// 最終に更新されたオブジェクトデータのみ返す
+const removeDuplicates = (array, property) => {
+    const uniqueArray = []
+  
+    array.forEach((item) => {
+        const value = item[property]
+        const index = uniqueArray.findIndex((uniqueItem) => uniqueItem[property] === value)
+        
+        if (index !== -1) {
+            uniqueArray.splice(index, 1);
+        }
+        uniqueArray.push(item)
+    })
+
+    return uniqueArray
+  }
+
+// 変更前のプロジェクト名の値を返す
+const before_project_name = (requestData) => {
+  const data = requestData.filter(val => val.project_name)
+
+  return data
+}
 
 if (sessionStorage.getItem('token') !== null) {
     resData = await request(requestAPI, sessionStorage)
+
+    // beforeData = before_project_name(JSON.parse(JSON.stringify(resDataCheckedAdd)))
+
+    for (let i = 0; i < resData.length; i++) {
+      let data = resData[i]
+      data["checked"] = false
+      resDataCheckedAdd.push(data)
+    }
+
+    // 変更前のデータを格納
+    beforeData = JSON.parse(JSON.stringify(resDataCheckedAdd))
+
+    // 監視処理
+    watch(resDataCheckedAdd, (newValue) => {
+      const maxLength = Math.max(newValue.length)
+      let checkedList = newValue.map((val) => val.checked)
+      let checkedListFlag = checkedList.filter(val => val === false)
+      for (let i = 0; i < maxLength; i++) {
+        if (newValue[i].checked === true) {
+          requestData.push(newValue[i])
+          deleteSaveFlag.value = true
+        } else if (checkedListFlag.length === maxLength) {
+          deleteSaveFlag.value = false
+        }
+      }
+    })
+
 
     // 評価指標画面に遷移
     evaluationindexPush = (val) => {
@@ -72,15 +131,70 @@ if (sessionStorage.getItem('token') !== null) {
         router.push('/objectmain/' + `?user_id=${id}`)
     }
 
-    save = () => {
-        console.log("crate botton")
+    save = async () => {
+      const req = removeDuplicates(requestData, "id")
+
+      const newRequest = req.map(val => {
+        return {
+          id: val.id, 
+          project_name: val.project_name, 
+          object_detection_model_name: val.object_detection_model_name_id
+        }
+      })
+
+      let request = {
+        "params": [
+        {
+          "username": username,
+          "token": token,
+          "user_id": id
+        }
+      ],
+        "data": newRequest
+    }
+
+    console.log(request)
+
+    await post(updateCreateAPI, request, router, currentRoute, 'update')
+    deleteSaveDialog.value = false
+
+    }
+
+    deleted = async () => {
+      const req = removeDuplicates(requestData, "id")
+
+      const newRequest = req.map(val => {
+        return {
+          id: val.id, 
+        }
+      })
+      let request = {
+        "params": [
+          {
+            "username": username,
+            "token": token,
+            "user_id": id
+          }
+        ],
+        "data": newRequest,
+      }
+      console.log(request)
+      await post(deleteAPI, request, router, currentRoute, 'delete')
+      deleteSaveDialog.value = false
     }
 
     close = () => {
         createDialog.value = false
+        if (removeDuplicates(requestData, "id").length) {
+          resDataCheckedAdd.splice(0, resDataCheckedAdd.length);
+          for (const val of beforeData) {
+            resDataCheckedAdd.push(val)
+          }
+        }
+        deleteSaveDialog.value= false
     }
 
-    create = () => {
+    create = async () => {
     let request = {
         "params": [
         {
@@ -93,7 +207,7 @@ if (sessionStorage.getItem('token') !== null) {
             {
                 "id": "0",
                 "project_name": project_name.value,
-                "object_detection_model_name_id": ""
+                "object_detection_model_name": ""
             }
         ]
     }
@@ -107,11 +221,11 @@ if (sessionStorage.getItem('token') !== null) {
     })
 
     if (allGetId.every(v => v === allGetId[0])) {
-        request["data"][0]["object_detection_model_name_id"] = allGetId[0]
+        request["data"][0]["object_detection_model_name"] = allGetId[0]
     }
-    console.log(request)
+
+    await post(updateCreateAPI, request, router, currentRoute, 'create')
     isEditing.value = !isEditing.value
-    hasSaved.value = true
     createDialog.value = false
     }
 
@@ -125,7 +239,7 @@ if (sessionStorage.getItem('token') !== null) {
       timer:3000
       })
       router.push('/auth')
-    }
+  }
 
 
 
@@ -183,12 +297,12 @@ if (sessionStorage.getItem('token') !== null) {
                         <span class="text-h5">登録情報</span>
                     </v-card-title>
                        <v-list
-                       v-for="(v, k) in detailsFrom"
-                       :key="k">
+                        v-for="(v, k) in detailsFrom"
+                        :key="k">
                         ■ {{ k }} : {{ v }}
                        </v-list>
                           <v-card-actions>
-                            <v-spacer></v-spacer>
+                            <v-spacer/>
                             <v-btn
                                 color="green"
                                 variant="text"
@@ -208,6 +322,7 @@ if (sessionStorage.getItem('token') !== null) {
       @click="returnScreen">
         前の画面に戻る
     </v-btn>
+    <!-- 新規追加処理 -->
     <v-row justify="center">
     <div class="text-center">
       <v-dialog
@@ -218,34 +333,29 @@ if (sessionStorage.getItem('token') !== null) {
             color="primary"
             v-bind="props"
           >
-          新規追加及び更新
+          新規追加
           </v-btn>
         </template>
-
         <v-card
           class="mx-auto"
           color="purple-lighten-1"
           max-width="800"
         >
         <v-toolbar width="500" color="purple">
-    
-            <v-toolbar-title>
+          <v-toolbar-title>
             <p>プロジェクト名新規追加</p>
-            </v-toolbar-title>
-    
-    
+          </v-toolbar-title>
             <v-btn
-                icon
-                @click="isEditing = !isEditing"
+              icon
+              @click="isEditing = !isEditing"
             >
-                <v-fade-transition leave-absolute>
-                    <v-icon v-if="isEditing">mdi-close</v-icon>
+              <v-fade-transition leave-absolute>
+                <v-icon v-if="isEditing">mdi-close</v-icon>
         
-                    <v-icon v-else>mdi-pencil</v-icon>
-                </v-fade-transition>
+                <v-icon v-else>mdi-pencil</v-icon>
+              </v-fade-transition>
             </v-btn>
         </v-toolbar>
-    
         <v-card-text>
             <v-text-field
             v-model="project_name"
@@ -255,38 +365,84 @@ if (sessionStorage.getItem('token') !== null) {
             ></v-text-field>
     
         </v-card-text>
-    
-        <v-divider></v-divider>
-    
-        <v-card-actions>
-            <v-spacer></v-spacer>
-                <v-btn
+        <v-divider/>
+          <v-card-actions>
+            <v-spacer/>
+              <v-btn
                 @click="close"
                 >
                 閉じる
-                </v-btn>
+              </v-btn>
         
-                <v-btn
+              <v-btn
                 :disabled="!isEditing"
                 @click="create"
                 >
                 登録
-                </v-btn>
-        </v-card-actions>
-    
-          <v-snackbar
-            v-model="hasSaved"
-            :timeout="2000"
-            attach
-            position="absolute"
-            location="bottom left"
-          >
-            新規追加
-          </v-snackbar>
+              </v-btn>
+          </v-card-actions>
         </v-card>
       </v-dialog>
     </div>
   </v-row>
+  <!-- 削除及び編集処理 -->
+    <v-dialog
+      v-model="deleteSaveDialog"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          編集
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title>
+          <span class="text-h8">プロジェクト名の更新及び削除</span>
+        </v-card-title>
+        <v-card-text
+          v-for="(item, index) in resDataCheckedAdd" 
+          :key="index"
+          :property="item.key"
+          :label="item.col">
+          <v-container>
+            <v-row>
+              <v-checkbox-btn
+                v-model="item.checked"
+                ></v-checkbox-btn>
+              <v-text-field
+                :disabled="!item.checked"
+                v-model="item.project_name"
+              ></v-text-field>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="primary" 
+            @click="close"
+          >閉じる
+          </v-btn>
+          <v-btn 
+            color="success" 
+            :disabled="!deleteSaveFlag" 
+            @click="save"
+          >保存
+          </v-btn>
+          <v-spacer/>
+          <v-btn 
+            color="blue-darken-1" 
+            variant="text" 
+            :disabled="!deleteSaveFlag" 
+            @click="deleted"
+          >削除
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 </template>
 
 
